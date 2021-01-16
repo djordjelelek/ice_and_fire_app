f273493d00075f7adf5c31b9784ef0fa43826228
Djordje Lelek <djordje.lelek@gmail.com>
2021-01-16 13:40:06 +0100

    update header for computer; change logo

--- src/Header/Menu/Menu.js
+++ src/Header/Menu/Menu.js
@@ -1,26 +1,13 @@
 import React, { useState } from "react";
 import classes from "./Menu.module.css";
 import menu from "./mobile-menu.png";
+import MenuItems from "./menuItems/MenuItems";
 
 const Menu = () => {
-  const [showMobileMenu, setShowingMobileMenu] = useState(false);
+  const [showMobileMenu, setShowingMobileMenu] = useState(true);
   const mobile_menu = showMobileMenu ? (
     <ul className={`${classes.List} ${classes.MobileMenu}`}>
-      <li link="/">
-        <a href="/">HOME</a>
-      </li>
-      <li link="/">
-        <a href="/">BOOKS</a>
-      </li>
-      <li link="/">
-        <a href="/">CHARACTERS</a>
-      </li>
-      <li link="/">
-        <a href="/">HOUSES</a>
-      </li>
-      <li link="/">
-        <a href="/">SEARCH</a>
-      </li>
+      <MenuItems />
     </ul>
   ) : null;
 
@@ -33,6 +20,9 @@ const Menu = () => {
         onClick={() => setShowingMobileMenu((previousState) => !previousState)}
       />
       {mobile_menu}
+      <ul className={`${classes.Menu} ${classes.MenuItem}`}>
+        <MenuItems />
+      </ul>
     </>
   );
 };
---
+++ src/Header/Menu/menuItems/MenuItems.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const menuItems = () => (
+  <>
+    <li link="/">
+      <a href="/">Home</a>
+    </li>
+    <li link="/">
+      <a href="/">Books</a>
+    </li>
+    <li link="/">
+      <a href="/">Characters</a>
+    </li>
+    <li link="/">
+      <a href="/">Houses</a>
+    </li>
+    <li link="/">
+      <a href="/">Search</a>
+    </li>
+  </>
+);
+
+export default menuItems;
